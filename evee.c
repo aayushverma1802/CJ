02f709cae692b37e03aa445c50af4a2ed09961b4
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-09-11 08:28:57 +0530

    Add files via upload

---
+++ evee.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a[]={12,23,4,7,8,9};
+    int n=sizeof(a)/sizeof(a[0]);
+    int k;
+    scanf("%d",&k);
+    for(int i=0;i<n;i++){
+        for(int j=1+i;j<n;j++){
+            if(k==a[i]+a[j]){
+                printf("Found\n");
+                printf("The number are:-\n%d\n%d\n",a[i],a[j]);
+                break;
+            }
+            
+        }
+    }
+    printf("All Done");
+
+    
+}
