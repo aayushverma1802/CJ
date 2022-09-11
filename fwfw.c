7fdb4b7976b40b20156ac37bc61baca0a9004610
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-09-11 08:27:10 +0530

    Add files via upload

---
+++ fwfw.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+void printArr(struct Node *head){
+    struct Node *p=head;
+    while(p!=NULL){
+        p=p->next;
+        printf("%d\n",p->data);
+    }
+}//void fuck()
+struct Node* can(struct Node* node){
+    struct Node * ptr=node;
+    ptr->data=ptr->next->data;
+    ptr->next=ptr->next->next;
+    return ptr;
+}
+int main(){
+    struct Node *head=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *s=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *t=(struct Node *)malloc(sizeof(struct Node));
+    struct Node *last=(struct Node *)malloc(sizeof(struct Node));
+    head->data=10;
+    s->data=11;
+    t->data=12;
+    last->data=13;
+    head->next=s;
+    s->next=t;
+    t->next=last;
+    last->next=s;
+    //head=can(s);
+    printArr(can(s));
+    //linkedListTraversal(head);
+    return 69;
+}
