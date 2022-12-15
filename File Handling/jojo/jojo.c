427e631f4910e9cd28a55ace5a32d3559bfea657
Aayush Verma <112875248+aayushverma1802@users.noreply.github.com>
2022-12-15 08:59:53 +0530

    First File Handling

---
+++ File Handling/jojo/jojo.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main()
+{
+   int num;
+   FILE *fptr;
+
+   fptr = fopen("C:\\program.txt","w");
+
+   if(fptr == NULL)
+   {
+      printf("Error!");   
+      exit(1);             
+   }
+
+   printf("Enter num: ");
+   scanf("%d",&num);
+
+   fprintf(fptr,"%d",num);
+   fclose(fptr);
+ 
+   if ((fptr = fopen("C:\\program.txt","r")) == NULL){
+       printf("Error! opening file");
+
+       exit(1);
+   }
+
+   fscanf(fptr,"%d", &num);
+
+   printf("Value of n=%d", num);
+   fclose(fptr);
+
+
+return 69;
+}
