35e72b24a710f8d49d747055cadd2ecb2368ccf4
Aayush Verma <aayush.verma2021@vitstudent.ac.in>
2023-05-13 13:05:27 +0530

    Add files via upload

---
+++ looef.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+       int n=adj->size();
+        vector<int>ans;
+        vector<bool>vis(V,false);
+        queue<int>q;
+        q.push(0);
+        vis[0]=true;
+        while(q.empty()==false){
+            int node=q.front();
+            ans.push_back(node);
+            q.pop();
+            for(auto x:adj[node]){
+                if(vis[x]==false){
+                    q.push(x);
+                    vis[x]=true;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
